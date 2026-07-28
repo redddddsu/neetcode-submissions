a8e43d8885fe312d8fa1e21013bb336c0e534226
Red Su <yredsu@gmail.com>
2026-07-28 16:05:52 +0800

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+
+        string concat = "";
+
+        for (int i = 0; i < strs.size(); i++) {
+            concat += strs[i];
+            concat += "1a3";
+        }
+        
+        return concat;
+
+    }
+
+    vector<string> decode(string s) {
+        string word = "";
+        vector<string> arr;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1' && s[i + 1] == 'a' && s[i + 2] == '3') {
+                i += 2;
+                arr.push_back(word);
+                word = "";
+            }
+            else
+                word += s[i];
+            
+
+        }
+        return arr;
+    }
+};
